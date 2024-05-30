4204b50e14387ad723cc75b7ec757d3be03325f3
SatriaRngga <h1101221037@student.untan.ac.id>
2024-05-30 14:39:22 +0700

    asascsa

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\ServiceProvider;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Paginator::useBootstrapFive();
+        require_once app_path('Helpers/helper.php');
+        view()->composer('*', function ($view) {
+            if (Auth::check()) {
+                View::share([
+                    'userGlobal' => User::find(Auth::user()->id),
+                    'judul' => 'E-Office',
+                    'footer' => 'E-Office Production 2024'
+                ]);
+            } else {
+                $view->with('userGlobal', null);
+            }
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,50 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\PermissionController;
 
-/* 
-|------------------------------------------------------------------------- - 
-| Web Routes 
-Yudarahma@gmail.com 
-|------------------------------------------------------------------------- - 
-| 
-| Here is where you can register web routes for your application. These | routes are loaded by the RouteServiceProvider and all of them will | be assigned to the "web" middleware group. Make something great! | 
+
+use Spatie\Permission\Models\Permission;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
 */
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
 Auth::routes();
+
+
 Route::middleware('auth')->group(function () {
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    Route::resources([
-        'roles' => RoleController::class,
-        'users' => UserController::class,
-        'products' => ProductController::class,
-        // 'permissions' => PermissionController::class, 
-    ]);
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class,'index'])->name('home');
+Route::get('/permission', [App\Http\Controllers\PermissionController::class,'index'])->name('permission');
+
+
+Route::resources([
+    'roles' => RoleController::class,
+    'users' => UserController::class,
+    'products' => ProductController::class,
+    'mahasiswa' => MahasiswaController::class,
+    'permissions' => PermissionController::class,
+]);
 });
